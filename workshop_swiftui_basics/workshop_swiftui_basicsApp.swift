511ea1e6275378310c0e1e814ebc83b4a71af899
Levent KANTAROĞLU <leventkantaroglu@dusyeri.com.tr>
2023-01-16 13:50:01 +0300

    Initial Commit

---
+++ workshop_swiftui_basics/workshop_swiftui_basicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshop_swiftui_basicsApp.swift
+//  workshop_swiftui_basics
+//
+//  Created by Levent Kantaroğlu on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct workshop_swiftui_basicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
